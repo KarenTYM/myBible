1e7a206eb7e22810206f84bcfa8e2efac33a86f9
Karen Tabuse Yoshimura Makihara <karen_yoshimura@yahoo.com.br>
2021-02-25 18:31:01 -0300

    Cria ViewControllers
    BibliaVC, DevocionalVC, OracaoVC

---
+++ myBible/myBible/Devocional/DevocionalVC.swift
@@ -0,0 +1,29 @@
+//
+//  DevocionalVC.swift
+//  myBible
+//
+//  Created by Fabio Makihara on 25/02/21.
+//
+
+import UIKit
+
+class DevocionalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ myBible/myBible/Oracao/OracaoVC.swift
@@ -0,0 +1,18 @@
+//
+//  OracaoVC.swift
+//  myBible
+//
+//  Created by Fabio Makihara on 25/02/21.
+//
+
+import UIKit
+
+class OracaoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
